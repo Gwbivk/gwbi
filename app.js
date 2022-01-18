ffdd76d28b94e166512ec29d17d873b9b6bee4da
Gwbivk <97592979+Gwbivk@users.noreply.github.com>
2022-01-17 19:13:14 -0800

    starting

--- app.js
+++ app.js
@@ -3,17 +3,18 @@ const path =require("path")
 
 const app =express()
 const mongoose = require('mongoose');
-const bodyparser = require( "body-parser")
+const bodyparser = require( "body-parser");
+const { Router } = require("express");
 
-// mongoose.connect(
-//     process.env.MONGODB_URI || 'mongodb://localhost/vircontact',
-//     {
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true,
-//       useCreateIndex: true,
-//       useFindAndModify: false,
-//     },
-//   );
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/vircontact',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    },
+  );
 
 
 const port = process.env.PORT || 80;
@@ -43,9 +44,48 @@ app.set('views',path.join(__dirname,'views'))
 
 
 //endpoints
-app.get('/',(req,res)=>{
-    const params ={}
-    res.status(200).render('home.pug',params);
+// app.get('/',(req,res)=>{
+//     const params ={}
+//     res.status(200).render('home.pug',params);
+
+// })
+// app.get('/com',(req,res)=>{
+//     const params ={}
+//     res.status(200).render('com.pug',params);
+
+// })
+
+// app.get('/contact',(req,res)=>{
+//     const params ={}
+//     res.status(200).render('contact.pug',params);
+
+// })
+
+
+// app.post('/contact', (req,res)=>{
+//     var  myData = new contact(req.body);
+//     myData.save().then(()=>{
+//         res.send("this oder has been saved ")
+       
+//      })
+    
+//  })
+
+app.use('/static',express.static('static'))
+ app.use(express.urlencoded())
+
+//pug specific stuff
+app.set('view engine','html')
+app.set('views',path.join(__dirname,'views'))
+
+
+//endpoints
+// app.get('/',(req,res)=>{
+//     // const params ={}
+//     res.status(200).render('index.html');
+app.get('/',function(req,res) {
+//  const params ={}
+     res.sendfile('./views/index.html');
 
 })
 app.get('/com',(req,res)=>{
@@ -54,12 +94,29 @@ app.get('/com',(req,res)=>{
 
 })
 
-app.get('/contact',(req,res)=>{
-    const params ={}
-    res.status(200).render('contact.pug',params);
 
-})
+//uncom
+// router.get('/',function(req,res) {
+//     res.sendfile(path.join(__dirname+'/index.html'));
 
+// })
+// app.get('/com',(req,res)=>{
+//     const params ={}
+//     res.status(200).render('com.pug',params);
+
+// })
+
+// app.get('/contact',(req,res)=>{
+//     // const params ={}
+//     res.status(200).render('contact.html');
+
+// })
+
+app.get('/contact',function(req,res) {
+    //  const params ={}
+         res.sendfile('./views/contact.html');
+    
+    })
 
 app.post('/contact', (req,res)=>{
     var  myData = new contact(req.body);
